8ac07bb54007605f5e94257f10fa495cbe7ee942
root <root@devscripts2ipv6.e2e.bos.redhat.com>
2021-01-14 11:19:40 +0000

    Adding support for ingress address

--- hack/virt-create-net.sh
+++ hack/virt-create-net.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
 sudo virsh net-create ./hack/net.xml
-echo server=/api.test-cluster.e2e.bos.redhat.com/192.168.126.1 | sudo tee /etc/NetworkManager/dnsmasq.d/aio.conf
+sudo tee -a /etc/NetworkManager/dnsmasq.d/aio.conf > /dev/null << EOL
+server=/api.test-cluster.e2e.bos.redhat.com/192.168.126.1
+address=/.apps.test-cluster.e2e.bos.redhat.com/192.168.126.10
+address=/api.test-cluster.e2e.bos.redhat.com/192.168.126.10
+address=/api-int.test-cluster.e2e.bos.redhat.com/192.168.126.10
+EOL
 echo -e "[main]\ndns=dnsmasq" | sudo tee /etc/NetworkManager/conf.d/aio.conf
 systemctl reload NetworkManager.service
